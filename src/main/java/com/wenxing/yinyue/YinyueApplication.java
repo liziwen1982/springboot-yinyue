0b36c058fbd2b4c7586ae11ea8897daeb2c5c058
liziwen <liziwen@richinfo.cn>
2024-03-27 15:11:53 +0800

    新建 springboot maven 项目

---
+++ src/main/java/com/wenxing/yinyue/YinyueApplication.java
@@ -0,0 +1,13 @@
+package com.wenxing.yinyue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YinyueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YinyueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wenxing/yinyue/YinyueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wenxing.yinyue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YinyueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
