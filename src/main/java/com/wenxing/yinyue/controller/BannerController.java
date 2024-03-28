873b6303a5baa864c7af35e169135eef41795dcb
liziwen <liziwen@richinfo.cn>
2024-03-28 14:15:17 +0800

    返回 banner json 数据

--- src/main/java/com/wenxing/yinyue/controller/BannerController.java
+++ src/main/java/com/wenxing/yinyue/controller/BannerController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class BannerController {
     @GetMapping("/banner")
     public String banner(){
-        return "banner";
+
+        String strRet = "{" +
+                "\"banners\":[" +
+                "{\"pic\":\"http://192.168.66.13/wechat/1.jpg\", \"bannerId\":\"1\"}," +
+                "{\"pic\":\"http://192.168.66.13/wechat/2.jpg\", \"bannerId\":\"2\"}," +
+                "{\"pic\":\"http://192.168.66.13/wechat/3.jpg\", \"bannerId\":\"3\"}" +
+                "]" +
+                "}";
+
+        System.out.println(strRet);
+        return strRet;
     }
 }
