2fa1c6d10125109c9717f79e43df79a1b716a9c0
liziwen <liziwen@richinfo.cn>
2024-03-28 15:31:34 +0800

    23 推荐歌曲动态实现

---
+++ src/main/java/com/wenxing/yinyue/controller/personlized.java
@@ -0,0 +1,24 @@
+package com.wenxing.yinyue.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class personlized {
+    @GetMapping("/personlized")
+    public String personlized(@RequestParam(name = "limit", defaultValue = "1") String limit){
+        String strRet = "{" +
+                "\"result\":[" +
+                "{\"picUrl\":\"http://192.168.66.13/wechat/1.jpg\", \"id\":\"1\", \"name\":\"好长的歌曲名1好长的歌曲名1好长的歌曲名1好长的歌曲名1好长的歌曲名1好长的歌曲名1\"}," +
+                "{\"picUrl\":\"http://192.168.66.13/wechat/2.jpg\", \"id\":\"2\", \"name\":\"好长的歌曲名1好长的歌曲名1好长的歌曲名1好长的歌曲名1好长的歌曲名1好长的歌曲名1\"}," +
+                "{\"picUrl\":\"http://192.168.66.13/wechat/3.jpg\", \"id\":\"3\", \"name\":\"好长的歌曲名1好长的歌曲名1好长的歌曲名1好长的歌曲名1好长的歌曲名1好长的歌曲名1\"}," +
+                "{\"picUrl\":\"http://192.168.66.13/wechat/4.png\", \"id\":\"4\", \"name\":\"好长的歌曲名1好长的歌曲名1好长的歌曲名1好长的歌曲名1好长的歌曲名1好长的歌曲名1\"}" +
+                "]" +
+                "}";
+
+        System.out.println(strRet);
+        System.out.println("limit:" + limit);
+        return strRet;
+    }
+}
