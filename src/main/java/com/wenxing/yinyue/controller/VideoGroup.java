4443928f729e4b87c08e7f17e2996c85ac61a7b9
liziwen <liziwen@richinfo.cn>
2024-04-02 16:49:13 +0800

    41 通过cookie获取视频数据

--- src/main/java/com/wenxing/yinyue/controller/VideoGroup.java
+++ src/main/java/com/wenxing/yinyue/controller/VideoGroup.java
@@ -1,12 +1,14 @@
 package com.wenxing.yinyue.controller;
 
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
 @RestController
-public class VideoGroupList {
+public class VideoGroup {
     @GetMapping("/video/group/list")
     public String videoGroupList(){
         StringBuilder strRet = new StringBuilder();
@@ -15,7 +17,7 @@ public class VideoGroupList {
                 "\"data\":[");
 
         for (int i = 0; i < 100; i++) {
-            strRet.append("{\"id\":\"" + i + "\", \"name\":\"现场" + i + "\"},");
+            strRet.append("{\"id\":\"" + i + "\", \"name\":\"视频组" + i + "\"},");
         }
 
         strRet = new StringBuilder(strRet.substring(0, strRet.length() - 1));
@@ -24,5 +26,12 @@ public class VideoGroupList {
         System.out.println(strRet);
 
         return strRet.toString();
+    }
+
+    @GetMapping("/video/group")
+    public String videoGroup(@RequestParam(name = "id", defaultValue = "") String id,
+                             @CookieValue(name = "cookie3", required = false) String cookie3){
+        System.out.println("cookie3:" + cookie3);
+        return "{\"code\":\"200\"}";
     }
 }
--- src/main/java/com/wenxing/yinyue/controller/login.java
+++ src/main/java/com/wenxing/yinyue/controller/login.java
@@ -1,5 +1,7 @@
 package com.wenxing.yinyue.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +12,8 @@ import java.util.Objects;
 public class login {
     @GetMapping("/login/cellphone")
     public String loginCellphone(@RequestParam(name = "phone", defaultValue = "") String phone,
-                                 @RequestParam(name = "password", defaultValue = "") String password){
+                                 @RequestParam(name = "password", defaultValue = "") String password,
+                                 HttpServletResponse response){
         System.out.println("phone:" + phone + "\npassword:" + password);
 
         String code = "0";
@@ -24,6 +27,31 @@ public class login {
         String strRet = STR."{\"code\":\{code},\"profile\":{\"msg\":\"profilemsg\", \"avatarUrl\":\"http://192.168.66.13/wechat/1.jpg\", \"nickname\":\"至尊宝\", \"userId\":\"1234512345\"}}";
 
         System.out.println(strRet);
+
+        Cookie myCookie = new Cookie("cookie1", "1");
+        // 设置Cookie的属性，如路径、域、有效期等
+        myCookie.setPath("/");
+        myCookie.setMaxAge(3600); // 设置为1小时（3600秒）
+        response.addCookie(myCookie);
+
+        myCookie = new Cookie("cookie2", "2");
+        // 设置Cookie的属性，如路径、域、有效期等
+        myCookie.setPath("/");
+        myCookie.setMaxAge(3600); // 设置为1小时（3600秒）
+        response.addCookie(myCookie);
+
+        myCookie = new Cookie("cookie3", "3");
+        // 设置Cookie的属性，如路径、域、有效期等
+        myCookie.setPath("/");
+        myCookie.setMaxAge(3600); // 设置为1小时（3600秒）
+        response.addCookie(myCookie);
+
+        myCookie = new Cookie("cookie4", "4");
+        // 设置Cookie的属性，如路径、域、有效期等
+        myCookie.setPath("/");
+        myCookie.setMaxAge(3600); // 设置为1小时（3600秒）
+        response.addCookie(myCookie);
+
         return strRet;
     }
 }
